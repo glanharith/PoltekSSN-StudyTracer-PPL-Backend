7a3965175d4216c401bbc76a20aa044e75f0220e
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2024-02-05 23:18:01 +0700

    add fetch data

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, PrismaService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    prismaService = app.get<PrismaService>(PrismaService);
+
+    await prismaService.user.upsert({
+      where: {
+        username: 'testuser1',
+      },
+      update: {},
+      create: {
+        username: 'testuser1',
+        name: 'Test User 1',
+      },
+    });
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('/hello/:username', () => {
+    it('should return "Hello {name}!" if username found', async () => {
+      expect(await appController.getUsername('testuser1')).toEqual(
+        'Hello Test User 1!',
+      );
+    });
+
+    it('should return "User not found" if username not found', async () => {
+      expect(await appController.getUsername('testuser2')).toEqual(
+        'User not found',
+      );
+    });
+  });
+
+  afterAll(async () => {
+    await prismaService.user.delete({
+      where: {
+        username: 'testuser1',
+      },
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getUsername(username: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (user) return `Hello ${user.name}!`;
+    return 'User not found';
   }
 }
