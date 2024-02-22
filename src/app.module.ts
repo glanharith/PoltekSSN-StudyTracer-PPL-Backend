8ebe4d66668a664e76d093c258e116da7000e742
sulthanftr <sulthanftr@gmail.com>
2024-02-22 17:23:53 +0700

    feat(studyProgramController): create and update study programs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { StudyProgramModule } from './studyProgram/studyProgram.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+    AuthModule,
+    StudyProgramModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ src/studyProgram/studyProgram.controller.spec.ts
@@ -0,0 +1,66 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { StudyProgram } from '@prisma/client';
+import { StudyProgramDTO } from './DTO';
+import { StudyProgramController } from './studyProgram.controller';
+import { StudyProgramService } from './studyProgram.service';
+
+jest.mock('./studyProgram.service');
+
+describe('StudyProgramController', () => {
+  let studyProgramController: StudyProgramController;
+  let studyProgramServiceMock: jest.Mocked<StudyProgramService>;
+
+  beforeEach(async () => {
+    const studyProgram: TestingModule = await Test.createTestingModule({
+      controllers: [StudyProgramController],
+      providers: [StudyProgramService],
+    }).compile();
+
+    studyProgramController = studyProgram.get<StudyProgramController>(
+      StudyProgramController,
+    );
+    studyProgramServiceMock =
+      studyProgram.get<jest.Mocked<StudyProgramService>>(StudyProgramService);
+  });
+
+  const studyProgramDTO: StudyProgramDTO = {
+    name: 'Computer Science',
+  };
+  const studyProgram: StudyProgram = {
+    id: '287ed51b-df85-43ab-96a3-13bb513e68c5',
+    name: 'Computer Science',
+  };
+
+  describe('POST /study-program', () => {
+    it('should create a new study program', async () => {
+      jest
+        .spyOn(studyProgramServiceMock, 'create')
+        .mockResolvedValue(studyProgram);
+      const result = await studyProgramController.createStudyProgram(
+        studyProgramDTO,
+      );
+      expect(result).toEqual({
+        message: 'Successfully created a new study program',
+      });
+    });
+  });
+
+  describe('UPDATE /study-program', () => {
+    it('should update a study program', async () => {
+      jest
+        .spyOn(studyProgramServiceMock, 'update')
+        .mockResolvedValue(studyProgram);
+      const result = await studyProgramController.updateStudyProgram(
+        studyProgram.id,
+        studyProgramDTO,
+      );
+      expect(result).toEqual({
+        message: 'Successfully updated a study program',
+      });
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
