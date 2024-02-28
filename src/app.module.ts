6c89101f0cd040d8af979270ad1ae27a5b7a6dfa
Glan Harith Teguh <glangulen@gmail.com>
2024-02-28 22:10:55 +0700

    [RED] finishing all test

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,8 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { StudyProgramModule } from './studyProgram/studyProgram.module';
-<<<<<<< HEAD
 import { ProfileModule } from './profile/profile.module';
-=======
 import { HeadOfStudyProgramModule } from './head-of-study-program/head-of-study-program.module';
->>>>>>> 4284ea96d8e8098843b4df8d0d56a98b763d0031
 
 @Module({
   imports: [
@@ -18,11 +15,8 @@ import { HeadOfStudyProgramModule } from './head-of-study-program/head-of-study-
     PrismaModule,
     AuthModule,
     StudyProgramModule,
-<<<<<<< HEAD
     ProfileModule,
-=======
     HeadOfStudyProgramModule,
->>>>>>> 4284ea96d8e8098843b4df8d0d56a98b763d0031
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
