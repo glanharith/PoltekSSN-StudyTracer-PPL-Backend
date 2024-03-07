ed02aa488f922cae385edb47867e239d1930f844
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2024-03-07 15:16:56 +0700

    [GREEN] (zxcvbn.service) feat: add implementation for ZxcvbnService

--- src/zxcvbn/zxcvbn.service.ts
+++ src/zxcvbn/zxcvbn.service.ts
@@ -1,10 +1,28 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { zxcvbn, zxcvbnAsync, zxcvbnOptions } from '@zxcvbn-ts/core';
+import * as zxcvbnCommonPackage from '@zxcvbn-ts/language-common';
+import * as zxcvbnEnPackage from '@zxcvbn-ts/language-en';
+import * as zxcvbnIdPackage from '@zxcvbn-ts/language-id';
+import { matcherPwnedFactory } from '@zxcvbn-ts/matcher-pwned';
 
 @Injectable()
 export class ZxcvbnService implements OnModuleInit {
-  onModuleInit() {}
+  onModuleInit() {
+    const matcherPwned = matcherPwnedFactory(fetch, zxcvbnOptions);
+    zxcvbnOptions.addMatcher('pwned', matcherPwned);
+    zxcvbnOptions.setOptions({
+      translations: zxcvbnEnPackage.translations,
+      graphs: zxcvbnCommonPackage.adjacencyGraphs,
+      dictionary: {
+        ...zxcvbnCommonPackage.dictionary,
+        ...zxcvbnEnPackage.dictionary,
+        ...zxcvbnIdPackage.dictionary,
+      },
+    });
+  }
 
   async getScore(password: string): Promise<number> {
-    return -1;
+    const { score } = await zxcvbnAsync(password);
+    return score;
   }
 }
