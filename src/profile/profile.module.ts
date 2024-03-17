ec24edc080ba91eae1d5ac288fe820904ab178b6
yudiptr <yudiputrasabri789@gmail.com>
2024-03-18 01:07:34 +0700

    improvement(edit password): add throw error when the new password is not strong

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ZxcvbnModule } from 'src/zxcvbn/zxcvbn.module';
 
 @Module({
+  imports: [ZxcvbnModule],
   controllers: [ProfileController],
   providers: [ProfileService, PrismaService],
 })
