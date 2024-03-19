d2002cb78783373c3075149614bbee0ec3c8df1a
Ahmad Hanif Adisetya <ahmad.hanif11@ui.ac.id>
2024-03-20 05:18:16 +0700

    [RED](SurveyControllerTest) feat: implement survey deletion test

--- src/survey/survey.controller.spec.ts
+++ src/survey/survey.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SurveyController } from './survey.controller';
 import { SurveyService } from './survey.service';
 import { CreateSurveyDTO } from './DTO/CreateSurveyDTO';
+import { InternalServerErrorException, NotFoundException } from '@nestjs/common';
 
 jest.mock('./survey.service');
 
@@ -41,6 +42,41 @@ describe('SurveyController', () => {
       const result = await surveyController.createSurvey(createSurveyDTO);
 
       expect(result).toEqual({ message: 'Survey successfully created' });
+    });
+  });
+
+  describe('DELETE /survey', () => {
+    const id = 'ba20eb7a-8667-4a82-a18d-47aca6cf84ef';
+    const nonExistentId = 'notExist';
+
+    it('should successfully delete a survey', async () => {
+      surveyServiceMock.delete.mockResolvedValue(id);
+      const result = await surveyController.delete(id);
+
+      expect(result).toEqual({
+        id,
+        message: 'Deleted survey successfully'
+      });
+
+      expect(surveyServiceMock.delete).toHaveBeenCalledWith(id);
+    });
+
+    it('should throw NotFoundException for a non-existing survey', async () => {
+      surveyServiceMock.delete.mockRejectedValue(new NotFoundException('Survey not found'));
+      
+      await expect(surveyController.delete(id)).rejects.toThrow(
+        NotFoundException
+      );
+    });
+
+    it('should handle errors during deletion', async () => {
+      surveyServiceMock.delete.mockRejectedValue(
+        new InternalServerErrorException('Error while deleting survey')
+      );
+
+      await expect(surveyController.delete(id)).rejects.toThrow(
+        InternalServerErrorException
+      );
     });
   });
 });
