ed4fc0390f8a6a2a177a69f1a5d636c7a2fbba31
sulthanftr <sulthanftr@gmail.com>
2024-03-23 18:54:39 +0700

    [RED] add tests for edit survey endpoint

--- src/survey/survey.controller.spec.ts
+++ src/survey/survey.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SurveyController } from './survey.controller';
 import { SurveyService } from './survey.service';
-import { CreateSurveyDTO } from './DTO/CreateSurveyDTO';
+import { CreateSurveyDTO, EditSurveyDTO } from './DTO/CreateSurveyDTO';
 
 jest.mock('./survey.service');
 
@@ -41,6 +41,44 @@ describe('SurveyController', () => {
       const result = await surveyController.createSurvey(createSurveyDTO);
 
       expect(result).toEqual({ message: 'Survey successfully created' });
+    });
+  });
+
+  describe('PATCH /survey', () => {
+    const surveyId = 'uuid';
+    const editSurveyDTO: EditSurveyDTO = {
+      title: 'title',
+      type: 'CURRICULUM',
+      description: 'description',
+      startTime: new Date(2024, 0, 1),
+      endTime: new Date(2024, 1, 1),
+      newQuestions: [
+        {
+          type: 'TEXT',
+          question: 'Question',
+          order: 2,
+        },
+      ],
+      updateQuestions: [
+        {
+          type: 'TEXT',
+          question: 'Question',
+          order: 1,
+        },
+      ],
+      deleteQuestions: [
+        {
+          id: 'uuid',
+        },
+      ],
+    };
+
+    it('should return success message', async () => {
+      surveyServiceMock.editSurvey.mockResolvedValue();
+
+      const result = await surveyController.editSurvey(surveyId, editSurveyDTO);
+
+      expect(result).toEqual({ message: 'Survey successfully updated' });
     });
   });
 });
