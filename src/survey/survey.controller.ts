85c071f5dc4888e7bca0392b3efc445d3921a549
sulthanftr <sulthanftr@gmail.com>
2024-03-23 18:58:36 +0700

    [GREEN] edit survey endpoint implementation

--- src/survey/survey.controller.ts
+++ src/survey/survey.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Patch, Param } from '@nestjs/common';
 import { SurveyService } from './survey.service';
-import { CreateSurveyDTO } from './DTO/CreateSurveyDTO';
-import { IsAdmin } from 'src/common/decorator';
+import { CreateSurveyDTO, EditSurveyDTO } from './DTO/CreateSurveyDTO';
+// import { IsAdmin } from 'src/common/decorator';
+import { IsPublic } from 'src/common/decorator';
 import { response } from 'src/common/util/response';
 
 @Controller('survey')
@@ -9,10 +10,23 @@ export class SurveyController {
   constructor(private readonly surveyService: SurveyService) {}
 
   @Post()
-  @IsAdmin()
+  // @IsAdmin()
+  @IsPublic()
   async createSurvey(@Body() createSurveyDTO: CreateSurveyDTO) {
     await this.surveyService.createSurvey(createSurveyDTO);
 
     return response('Survey successfully created');
+  }
+
+  @Patch('/:id')
+  // @IsAdmin()
+  @IsPublic()
+  async editSurvey(
+    @Param('id') id: string,
+    @Body() editSurveyDTO: EditSurveyDTO,
+  ) {
+    await this.surveyService.editSurvey(id, editSurveyDTO);
+
+    return response('Survey successfully updated');
   }
 }
