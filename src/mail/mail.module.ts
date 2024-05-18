ddc4c1263f10bbcd0fad5f8ef4e0e0027a66f7e9
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2024-05-18 21:47:54 +0700

    (MailService) fix: remove unnecessary module declaration

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { MailerService } from '@nestjs-modules/mailer';
 
 @Module({
-  providers: [MailService, MailerService],
+  providers: [MailService],
   exports: [MailService],
 })
 export class MailModule {}
