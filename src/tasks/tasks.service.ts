d8f54daf1bf4a366699becb0fd1f525dfe5587ec
Ahmad Hanif Adisetya <ahmad.hanif11@ui.ac.id>
2024-05-20 17:45:27 +0700

    [GREEN](TasksService) feat: implement automation survey active check

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+
+  constructor(private prisma: PrismaService) {}
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async updateFormActivityStatus() {
+    this.logger.log('Updating form activity status');
+    const now = new Date();
+
+    await this.prisma.form.updateMany({
+      where: {
+        isActive: false,
+        startTime: { lte: now },
+        endTime: { gte: now },
+        lastUpdate: null
+      },
+      data: { isActive: true }
+    });
+
+    const formsToDeactivate = await this.prisma.form.findMany({
+      where: {
+        isActive: true,
+        endTime: { lt: now }
+      }
+    });
+
+    if (Array.isArray(formsToDeactivate)){
+      for (const form of formsToDeactivate) {
+        if (!form.lastUpdate || form.lastUpdate < form.endTime) {
+          await this.prisma.form.update({
+            where: { id: form.id },
+            data: { isActive: false }
+          });
+        }
+      }
+    }
+  }
+}
